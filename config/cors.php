42ae3491f38d2e6153d8a6febc7269af397857ce
Pourya <bakhshian2020@gmail.com>
2025-11-27 13:49:36 +0330

    repair register page

--- config/cors.php
+++ config/cors.php
@@ -7,11 +7,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-
-        'http://127.0.0.1:4173',
-        'http://localhost:4173',
+        'https://sa-bt.ir', 
+        'https://www.sa-bt.ir',
     ],
 
     'allowed_origins_patterns' => [],
