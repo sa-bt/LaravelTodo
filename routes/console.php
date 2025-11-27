33a4e1d78ab75d897f223249c91eb369e58db373
Pourya <bakhshian2020@gmail.com>
2025-11-27 22:28:32 +0330

    fix schedule bug

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+        Schedule::command('reports:send-due')->everyMinute()->withoutOverlapping();
+        Schedule::command('tasks:send-due')->everyMinute()->withoutOverlapping();
+        Schedule::command('app:send-goal-reminders')
+            ->everyMinute()
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/scheduler-reminders.log'));
+
+    
